8feb89ebb1016e56d29f0e69bf9e66ae9b8498ec
Pierre Ellul <7912093+pellul@users.noreply.github.com>
2021-05-21 01:26:12 +0200

    Update screen locked image and dirty attempt to make it seamless

--- i3/.config/i3/scripts/i3lock.sh
+++ i3/.config/i3/scripts/i3lock.sh
@@ -2,10 +2,22 @@
 img=$(mktemp /tmp/XXXXXXXXXX.png)
 
 # screenshot
-import -window root $img
+import -window root $img &
+shot_pid=$!
 
-convert $img -scale 10% -scale 1000% $img
+# temporary lock
+i3lock -n -c '#1B262C' -u &
+lock_pid=$!
 
-i3lock -t -u -i $img
+# wait for the screenshot to end before processing it with ImageMagick
+while [ -n "$shot_pid" -a -e /proc/$shot_pid ]
+do
+sleep 0.1
+done
+
+convert $img -scale 10% -scale 1000% -quality 1 miff:- |\
+composite -gravity Southwest $HOME/.config/i3/scripts/chira.png miff:- $img &&
+kill $lock_pid
+i3lock -n -t -u -i $img
 
 rm $img
