0278f16cb415e75e945809921ff39aeac65714dd
Pierre Ellul <pierre.ellul@jaguar-network.com>
2021-06-25 18:32:21 +0200

    random waifu i3lock

--- i3/.config/i3/scripts/i3lock.sh
+++ i3/.config/i3/scripts/i3lock.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+pushd $HOME/.config/i3/scripts
 img=$(mktemp /tmp/XXXXXXXXXX.png)
+
+waifu=$(find ./images -maxdepth 1 -type f -printf "%f\n" | sort -R | head -1)
+gravity=${waifu%_*}
 
 # screenshot
 import -window root $img &
@@ -12,12 +17,13 @@ lock_pid=$!
 # wait for the screenshot to end before processing it with ImageMagick
 while [ -n "$shot_pid" -a -e /proc/$shot_pid ]
 do
-sleep 0.1
+    sleep 0.1
 done
 
-convert $img -scale 10% -scale 1000% -quality 1 miff:- |\
-composite -gravity Southwest $HOME/.config/i3/scripts/chira.png miff:- $img &&
-kill $lock_pid
+convert $img -scale 10% -scale 1000% -quality 1 miff:- | \
+    composite -gravity $gravity ./images/$waifu miff:- $img &&
+    kill $lock_pid
 i3lock -n -t -u -i $img
 
 rm $img
+popd
