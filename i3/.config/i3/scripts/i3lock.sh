d96b4841b31797817da17c6c539938378a0303de
Pierre Ellul <7912093+pellul@users.noreply.github.com>
2022-05-18 23:13:32 +0200

    Leaving kitty for alacritty
    Minor changes to the i3lock script

--- i3/.config/i3/scripts/i3lock.sh
+++ i3/.config/i3/scripts/i3lock.sh
@@ -7,18 +7,7 @@ waifu=$(find ./images -maxdepth 1 -type f -printf "%f\n" | sort -R | head -1)
 gravity=${waifu%_*}
 
 # screenshot
-import -window root $img &
-shot_pid=$!
-
-# temporary lock
-i3lock -n -c '#1B262C' -u &
-lock_pid=$!
-
-# wait for the screenshot to end before processing it with ImageMagick
-while [ -n "$shot_pid" -a -e /proc/$shot_pid ]
-do
-    sleep 0.1
-done
+import -window root $img
 
 # quote=$(cat ./quotes/kaamelott.txt | sort -R | head -1 | fold -sw 75)
 # minfontsize=30
@@ -29,8 +18,7 @@ convert $img \
     -scale 10% -scale 1000% -quality 1 miff:- |\
     # -gravity NorthWest -fill black -pointsize $fontsize -annotate +103+153 "$quote" \
     # -gravity NorthWest -fill white -pointsize $fontsize -annotate +100+150 "$quote" miff:- | \
-    composite -gravity $gravity ./images/$waifu miff:- $img &&
-    kill $lock_pid
+    composite -gravity $gravity ./images/$waifu miff:- $img
 
 #./quotes/quotes.sh &
 #quotes_pid=$!
